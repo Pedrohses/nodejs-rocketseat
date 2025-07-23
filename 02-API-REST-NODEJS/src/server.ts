d12560ddddf263854051457a06d4fec6bfb05a58
Pedrohses <desouzaesilvap@gmail.com>
2025-07-22 22:09:26 -0300

    feat: create a database and configure using migrations with knex query builder and configure enviroment variables

--- 02-API-REST-NODEJS/src/server.ts
+++ 02-API-REST-NODEJS/src/server.ts
@@ -1,14 +1,20 @@
 import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')

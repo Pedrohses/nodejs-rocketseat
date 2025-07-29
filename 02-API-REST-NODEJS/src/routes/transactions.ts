720e3d3f3f72ac6bc8eabe410c53072dfc8ae779
Pedrohses <desouzaesilvap@gmail.com>
2025-07-28 22:08:14 -0300

    feat: add unit tests for improment

--- 02-API-REST-NODEJS/src/routes/transactions.ts
+++ 02-API-REST-NODEJS/src/routes/transactions.ts
@@ -59,7 +59,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
         .sum('amount', { as: 'amount' })
         .first()
 
-      return summary
+      return { summary }
     },
   )
 
